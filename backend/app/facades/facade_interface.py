9813c52906b8836f2195f86d84e8a1bddc290741
Marconi Gomes <marconi.gomes7@gmail.com>
2023-08-15 22:54:17 -0300

    patterns(singleton): Implementation

--- backend/app/facades/facade_interface.py
+++ backend/app/facades/facade_interface.py
@@ -1,8 +1,15 @@
 from app.delegate.delegate_model import Delegate
 from app.institution.institution_model import Institution
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class FacadeInterface:
+
+class FacadeInterface(metaclass=Singleton):
 
     @staticmethod
     def do_create_institution(institution: Institution) -> bool:
